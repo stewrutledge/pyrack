0b9f4de9dc69fda736eba6dbac22687c740af7b1
Stewart Rutledge <stewart.rutledge@kb.se>
2014-01-17 09:58:07 +0100

    Added wsgi support to web_api.py so it can be ran either standalone or within a virtual host

--- web_api.py
+++ web_api.py
@@ -1,27 +1,33 @@
 #coding=utf-8
-from bottle import route, run
-from pyrack import RackConnect, RackObjects
+if __name__ != '__main__':
+    from sys import path as syspath
+    from os import path, chdir
+    syspath.append(path.dirname(__file__))
+    chdir(path.dirname(__file__))
 
+from bottle import route, run, default_app, Bottle
+from pyrack import RackConnect, RackObjects
 
 rackdoc = RackConnect(mysql_host='localhost', user='rack', password='rack', database='rack_test')
 rackobjects = RackObjects(rackdoc)
+app = Bottle()
 
 @route('/facts/:obj_id')
-def facts(obj_id=None):
+def facts( obj_id=None):
 #    try:
-        obj_id = int(obj_id)
-        return rackobjects.obj_attr(obj_id)
+    obj_id = int(obj_id)
+    return rackobjects.obj_attr(obj_id)
 #    except ValueError:
 #        return {'message': 'Object ID must be an integer'}
 
 @route('/name/:name')
 def by_name(name=None):
 #    try:
-        obj_id = int(rackobjects.get_id(from_type='name', value=name))
-        return {'RackObj': rackobjects.obj_attr(obj_id)}
+    obj_id = int(rackobjects.get_id(from_type='name', value=name))
+    return {'RackObj': rackobjects.obj_attr(obj_id)}
 
 @route('/fqdn/:fqdn')
-def by_fqdn(fqdn=None):
+def by_fqdn( fqdn=None):
     obj_id = int(rackobjects.get_id(from_type='FQDN', value=fqdn))
     return rackobjects.obj_attr(obj_id)
 #    except:
@@ -33,4 +39,7 @@ def with_role(role_id=None,env=None):
         return rackobjects.with_role(role_id=role_id, environment=env)
     except:
         return {}
-run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    run(host='0.0.0.0', port=8080)
+if __name__ != '__main__':
+    application = default_app()
