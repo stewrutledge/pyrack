307797d1c26af3d4edf5170b63af8904aa377f23
Stewart Rutledge <stewart.rutledge@kb.se>
2014-01-27 11:15:16 +0100

    Removed some lines that were commented out anyway

--- web_api.py
+++ web_api.py
@@ -14,15 +14,11 @@ app = Bottle()
 
 @route('/facts/:obj_id')
 def facts( obj_id=None):
-#    try:
     obj_id = int(obj_id)
     return rackobjects.obj_attr(obj_id)
-#    except ValueError:
-#        return {'message': 'Object ID must be an integer'}
 
 @route('/name/:name')
 def by_name(name=None):
-#    try:
     obj_id = int(rackobjects.get_id(from_type='name', value=name))
     return {'RackObj': rackobjects.obj_attr(obj_id)}
 
@@ -30,8 +26,6 @@ def by_name(name=None):
 def by_fqdn( fqdn=None):
     obj_id = int(rackobjects.get_id(from_type='FQDN', value=fqdn))
     return rackobjects.obj_attr(obj_id)
-#    except:
-#        return {'RackObj': 'could not find name!'}
 
 @route('/withrole/:env/:role_id')
 def with_role(role_id=None,env=None):
