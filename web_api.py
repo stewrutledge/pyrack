8664607d48f08c32d10a8378a686d2ae24909192
sterutkb <stewart.rutledge@kb.se>
2014-07-30 09:20:57 +0200

    Further cleaning and addition of several search parameters

--- web_api.py
+++ web_api.py
@@ -16,32 +16,41 @@ db_user = rackdb.get('mysql', 'user')
 db_pass = rackdb.get('mysql', 'password')
 db_name = rackdb.get('mysql', 'dbname')
 
-rackdoc = RackConnect(mysql_host=db_host, user=db_user, password=db_pass, database=db_name)
+rackdoc = RackConnect(
+    mysql_host=db_host,
+    user=db_user,
+    password=db_pass,
+    database=db_name
+)
 rackobjects = RackObjects(rackdoc)
 app = Bottle()
 
+
 @route('/facts/:obj_id')
-def facts( obj_id=None):
+def facts(obj_id=None):
     obj_id = int(obj_id)
     return rackobjects.obj_attr(obj_id)
+
 
 @route('/name/:name')
 def by_name(name=None):
     obj_id = int(rackobjects.get_id(from_type='name', value=name))
     return {'RackObj': rackobjects.obj_attr(obj_id)}
 
+
 @route('/fqdn/:fqdn')
-def by_fqdn( fqdn=None):
+def by_fqdn(fqdn=None):
     obj_id = int(rackobjects.get_id(from_type='FQDN', value=fqdn))
     return rackobjects.obj_attr(obj_id)
 
+
 @route('/withrole/:env/:role_id')
-def with_role(role_id=None,env=None):
-    try:
+def with_role(role_id=None, env=None):
+#    try:
         return rackobjects.with_role(role_id=role_id, environment=env)
-    except Exception as e:
-        return {"error": e.message}
+#    except Exception as e:
+#        return {"error": e.message}
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=8080)
+    run(host='0.0.0.0', port=8282)
 if __name__ != '__main__':
     application = default_app()
