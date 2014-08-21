b075a15fb425c35988eee13a04fab91ffb7459e5
sterutkb <stewart.rutledge@kb.se>
2014-08-21 14:23:47 +0200

    Significant code cleanup attempt

--- pyrack.py
+++ pyrack.py
@@ -14,12 +14,18 @@ class RackError:
 
 
 class _RackAPI:
-    def __init__(self, mysql_host, user, password, database, charset='utf8'):
+    def __init__(
+            self, mysql_host,
+            user, password,
+            database, charset='utf8',
+            **kw):
         self._conn = {'host': mysql_host,
                       'user': user,
                       'pass': password,
                       'db': database,
                       'charset': charset}
+        self.fqdnID = kw.get('fqdnID', 10003)
+        self.ansibleTag = kw.get('ansibleTag', 3)
 
     def _connect(self, connection):
         conn = connect(
@@ -34,7 +40,8 @@ class _RackAPI:
     def _get_dict(self, dict_key):
         self._connect(self._conn)
         self._cur.execute(
-            "SELECT dict_value from Dictionary where dict_key = %s" % dict_key
+            "SELECT dict_value from Dictionary where dict_key = %s",
+            (dict_key, )
         )
         resp = self._cur.fetchall()
         self._cur.close()
@@ -43,7 +50,7 @@ class _RackAPI:
     def _get_object(self, obj_id):
         self._connect(self._conn)
         self._cur.execute(
-            "SELECT name, asset_no FROM Object where id = %s" % obj_id
+            "SELECT name, asset_no FROM Object where id = %s", (obj_id, )
         )
         resp = self._cur.fetchall()
         data = {}
@@ -69,7 +76,7 @@ class _RackAPI:
         networks = [(['%s/%s' % (net[0], net[1]), net[2]]) for net in networks]
         self._cur.execute("""
             SELECT INET_NTOA(ip) from
-            IPv4Allocation where object_id = %s""" % obj_id)
+            IPv4Allocation where object_id = %s""", (obj_id, ))
         try:
             ipv4 = self._cur.fetchall()[0][0]
             ipv4_obj = ip_address(unicode(ipv4))
@@ -87,13 +94,16 @@ class _RackAPI:
                               from IPv4Allocation
                               where INET_NTOA(ip)
                               LIKE '%s.%s'
-                              """ % ((".").join(ipv4.split('.')[:+3]), '%')
+                              """, ((".").join(ipv4.split('.')[:+3]), '%')
                 self._cur.execute(router_sql)
                 routers = self._cur.fetchall()
             for network in networks:
                 if ipv4_obj in ip_network(unicode(network[0])):
                     ipv4net = ip_network(unicode(network[0]))
-                    aton = struct.unpack("!I", inet_aton(ipv4net.network_address.exploded))[0]
+                    aton = struct.unpack(
+                        "!I",
+                        inet_aton(ipv4net.network_address.exploded)
+                    )[0]
                     self._cur.execute("""
                         SELECT id FROM IPv4Network
                         WHERE ip = %s
@@ -103,7 +113,7 @@ class _RackAPI:
                              VLANIPv4 where ipv4net_id = %s
                             """, (ipq_res[0], ))
                     vlanq_res = self._cur.fetchone()
-                    self._cur.execute("""SELECT vlan_descr FROM 
+                    self._cur.execute("""SELECT vlan_descr FROM
                                    VLANDescription WHERE
                                    vlan_id = %s""", (vlanq_res,))
                     vlannameq_res = self._cur.fetchone()
@@ -113,15 +123,12 @@ class _RackAPI:
                         'ipv4': ipv4,
                         'vlanName': vlannameq_res[0]
                     }
-#                    net_id = network[1]
                     _network = ip_network(unicode(network[0]))
                     break
-#            gateways = []
             for router in routers:
                 if ip_address(unicode(router[0])) in _network:
                     payload['gateway'] = router[0]
                     break
-            print payload
             return payload
         except Exception as e:
             print e
@@ -144,7 +151,8 @@ class _RackAPI:
         self._connect(self._conn)
         try:
             self._cur.execute(
-                "SELECT tag_id from TagStorage where entity_id = %s" % obj_id
+                "SELECT tag_id from TagStorage where entity_id = %s",
+                (obj_id, )
             )
             resp = self._cur.fetchall()
         except:
@@ -158,7 +166,7 @@ class _RackAPI:
         self._connect(self._conn)
         self._cur.execute("""
             SELECT attr_id, string_value, uint_value
-            from AttributeValue where object_id = %s""" % obj_id)
+            from AttributeValue where object_id = %s""", (obj_id, ))
         resp = self._cur.fetchall()
         if len(resp) > 0:
             obj_attr = {}
@@ -184,10 +192,12 @@ class _RackAPI:
                 obj_attr['ipv4']['vlan'] = network_info['vlanName']
                 obj_attr['obj_id'] = obj_id
                 if attr_type == 'dict':
+                    obj_attr[attr_name] = self._get_dict(item[2])[0]
                     try:
-                        obj_attr[attr_name] = self._get_dict(item[2])[0].split("%")[2]
+                        obj_attr[attr_name] = obj_attr[attr_name].split("%")[2]
                     except:
-                        obj_attr[attr_name] = self._get_dict(item[2])[0]
+#                        obj_attr[attr_name] = self._get_dict(item[2])[0]
+                        pass
                 else:
                     try:
                         attr_list = item[1].split(",")
@@ -207,18 +217,23 @@ class _RackAPI:
 
     def _get_tag_id(self, tagString):
         self._connect(self._conn)
-        self._cur.execute('select id from TagTree where tag = %s', (tagString, ))
+        self._cur.execute(
+            'select id from TagTree where tag = %s', (tagString, )
+        )
         resp = self._cur.fetchone()
         if resp:
             return resp[0]
         else:
             return None
-    
+
     def _with_tag(self, tagString):
         tagID = self._get_tag_id(tagString)
-        tagList = []
+#        tagList = []
         if tagID is not None:
-            self._cur.execute('select entity_id from TagStorage where tag_id = %s', (tagID, ))
+            self._cur.execute(
+                'select entity_id from TagStorage where tag_id = %s',
+                (tagID, )
+            )
             tresp = self._cur.fetchall()
         else:
             tresp = ''
@@ -227,8 +242,7 @@ class _RackAPI:
             query = ("""
                 SELECT name
                 FROM Object
-                WHERE id in (%s)"""
-            )
+                WHERE id in (%s)""")
             inTransform = ', '.join(map(lambda x: '%s', searchIDs))
             query = query % inTransform
             self._cur.execute(query, searchIDs)
@@ -239,12 +253,13 @@ class _RackAPI:
             return([name[0] for name in withTagResp])
         else:
             return([])
-  
+
     def _with_role(self, role_id=None, environment=None):
         role_dict = self._gen_role_dict()
         self._connect(self._conn)
         self._cur.execute(
-            "SELECT dict_key from Dictionary where dict_value = %s", environment
+            "SELECT dict_key from Dictionary where dict_value = %s",
+            (environment, )
         )
         env_resp = self._cur.fetchall()
         if len(env_resp) == 0:
@@ -259,7 +274,9 @@ class _RackAPI:
                   SELECT id from `TagTree` where parent_id = %s
                 )
              AND entity_id = ANY (
-            select object_id from AttributeValue where uint_value = %s)""", (role_id, env_id)) 
+            select object_id from AttributeValue where uint_value = %s)""",
+            (role_id, env_id)
+        )
         role_resp = self._cur.fetchall()
         if len(role_resp) == 0:
             raise KeyError("No hosts found")
@@ -267,13 +284,12 @@ class _RackAPI:
         fqdns = {}
         self._connect(self._conn)
         searchIDs = [int(obj_id[0]) for obj_id in role_resp]
-        
+
         #print searchIDs
         query = ("""
             SELECT name, id
             FROM Object
-            WHERE id in (%s)"""
-        )
+            WHERE id in (%s)""")
         inTransform = ', '.join(map(lambda x: '%s', searchIDs))
         query = query % inTransform
         self._cur.execute(query, searchIDs)
@@ -285,7 +301,7 @@ class _RackAPI:
         #print fqdns
         for role in role_resp:
           #  print role
-            try: 
+            try:
                 roles[role_dict[role[1]]]
             except KeyError:
                 roles[role_dict[role[1]]] = []
@@ -295,24 +311,27 @@ class _RackAPI:
         hostVars = roles['_meta']['hostvars']
         for obj_id in searchIDs:
             network_info = self._ipv4(obj_id)
-            hostVars[fqdns[obj_id]] = {'ansible_ssh_host': network_info['ipv4']}
+            hostVars[fqdns[obj_id]] = {
+                'ansible_ssh_host': network_info['ipv4']
+            }
         for obj_id in searchIDs:
-            self._cur.execute('select string_value from AttributeValue where object_id = %s and attr_id = 10003', (obj_id, ))
+            self._cur.execute("""
+                select string_value from AttributeValue where
+                object_id = %s and attr_id = %s""", (obj_id, self.fqdnID))
             resp = self._cur.fetchone()
             print self._cur._last_executed
-        
+
             print resp
             if resp:
                 extraVars = resp[0].split()
                 for var in extraVars:
-                    k,v = var.split('=')
+                    k, v = var.split('=')
                     hostVars[fqdns[obj_id]][k] = v
-            
         return roles
 
     def _name_to_id(self, name=None):
         self._connect(self._conn)
-        self._cur.execute("SELECT id FROM Object WHERE name = '%s'" % name)
+        self._cur.execute("SELECT id FROM Object WHERE name = '%s'", (name, ))
         resp = self._cur.fetchone()
         self._cur.close()
         if len(resp) == 0:
@@ -321,7 +340,11 @@ class _RackAPI:
 
     def _fqdn_to_id(self, fqdn=None):
         self._connect(self._conn)
-        self._cur.execute("SELECT object_id FROM AttributeValue WHERE string_value = '%s' and attr_id = 3" % fqdn)
+        self._cur.execute("""
+            SELECT object_id FROM AttributeValue WHERE string_value = '%s'
+            and attr_id = %s
+            """, (fqdn, self.ansibleTag)
+        )
         resp = self._cur.fetchone()
         self._cur.close()
         if len(resp) == 0:
@@ -330,17 +353,32 @@ class _RackAPI:
 
     def _get_dns(self, obj_id=None):
         self._connect(self._conn)
-        self._cur.execute("SELECT INET_NTOA(ip) from IPv4Allocation where object_id = %s" % obj_id)
+        self._cur.execute("""
+            SELECT INET_NTOA(ip) from
+            IPv4Allocation where object_id = %s
+            """, (obj_id, )
+        )
         resp = self._cur.fetchall()
         if ip_address(unicode(resp[0][0])).is_private:
             tag_id = 3
         if not ip_address(unicode(resp[0][0])).is_private:
             tag_id = 4
-        self._cur.execute("SELECT entity_id from TagStorage where tag_id = %s" % tag_id)
+        self._cur.execute("""
+            SELECT entity_id from
+            TagStorage where tag_id = %s
+            """, (tag_id, )
+        )
         resp = self._cur.fetchall()
-        obj_list = ["object_id = %s" % obj[0] for obj in resp]
-        with_ors = " OR ".join(obj_list)
-        self._cur.execute("SELECT INET_NTOA(ip) FROM IPv4Allocation WHERE %s" % with_ors)
+        #obj_list = ["object_id = %s" % obj[0] for obj in resp]
+        obj_list = [obj[0] for obj in resp]
+        inTransform = ', '.join(map(lambda x: '%s', obj_list))
+        #with_ors = " OR ".join(obj_list)
+        query = ("""
+            SELECT INET_NTOA(ip) FROM
+            IPv4Allocation WHERE %s object_id in (%s)
+        """)
+        query = query % inTransform
+        self._cur.execute(query, obj_list)
         resp = self._cur.fetchall()
         self._cur.close()
         return [ipv4[0] for ipv4 in resp]
@@ -348,7 +386,12 @@ class _RackAPI:
 
 class RackConnect(_RackAPI):
     def RackConnect(self, mysql_host, user, password, database):
-        _RackAPI.__init__(mysql_host=mysql_host, user=user, password=password, database=database)
+        _RackAPI.__init__(
+            mysql_host=mysql_host,
+            user=user,
+            password=password,
+            database=database
+        )
 
 
 class RackObjects(object):
@@ -367,10 +410,8 @@ class RackObjects(object):
         elif isinstance(obj_id, int):
             return self.rack._get_attributes(obj_id)
 
-
     def with_tag(self, tagString):
         return(self.rack._with_tag(tagString))
-
 
     def with_role(self, role_id=None, environment=None):
         return self.rack._with_role(role_id=role_id, environment=environment)
